b33cba8819489d6fa44d5cb3d8b06692eded5be5
Jon Doe <jon.doe.5dec@gmail.com>
2025-08-21 12:37:16 +0000

    just undo what you have done in previous chat , because screen i white

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 
 import type {NextConfig} from 'next';
 
-const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX || undefined;
-
 const nextConfig: NextConfig = {
-  assetPrefix,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
