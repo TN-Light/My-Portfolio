8f670dd29a055c3736a7f6c06035dee2d403dff7
Jon Doe <jon.doe.5dec@gmail.com>
2025-08-21 13:20:11 +0000

    my logo is fire a blue fire like firebase

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,18 +4,18 @@ export const LogoIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="none"
+    fill="currentColor"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth="1"
     strokeLinecap="round"
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M12 2L2 7l10 5 10-5-10-5z" />
-    <path d="M2 17l10 5 10-5" />
-    <path d="M2 12l10 5 10-5" />
+    <path d="M12.83,3.06C12.5,2.8,12.12,3,12.06,3.4C11.12,9.37,6.66,12.6,3.42,14.54C2.9,14.86,3.13,15.54,3.69,15.54C8.2,15.54,11.08,13.1,13,10.64C14.12,12.7,16.8,15.54,20.31,15.54C20.87,15.54,21.1,14.86,20.58,14.54C17.34,12.6,12.88,9.37,11.94,3.4C11.88,3,11.5,2.8,11.17,3.06" />
+    <path d="M12,17.27C10.22,17.27,8.8,18.33,8.38,19.66C8.23,20.09,8.6,20.54,9.07,20.37C10.15,19.92,11.08,19.66,12,19.66C12.92,19.66,13.85,19.92,14.93,20.37C15.4,20.54,15.77,20.09,15.62,19.66C15.2,18.33,13.78,17.27,12,17.27Z" />
   </svg>
 );
+
 
 export const GithubIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
