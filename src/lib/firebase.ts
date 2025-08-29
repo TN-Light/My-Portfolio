2e8770e9ba2c7575c39b689d818f2fe3711e782e
Jon Doe <jon.doe.5dec@gmail.com>
2025-08-29 05:33:40 +0000

    i gave the genai key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,6 @@ const firebaseConfig = {
   projectId: "neondepth-portfolio",
   appId: "1:870342837142:web:b974911d6597ee7ff9d3b7",
   storageBucket: "neondepth-portfolio.firebasestorage.app",
-  apiKey: "AIzaSyBASX-wRtWWMElzkGrErfGNCfl3JYpYS5w",
   authDomain: "neondepth-portfolio.firebaseapp.com",
   messagingSenderId: "870342837142",
 };
